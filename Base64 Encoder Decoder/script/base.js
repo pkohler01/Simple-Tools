2b0614d2c445a4cc415f0e9803131b4ab1ec9039
pkohler01 <86849865+pkohler01@users.noreply.github.com>
2021-07-27 13:34:35 -0400

    Add files via upload

---
+++ Base64 Encoder Decoder/script/base.js
@@ -0,0 +1,20 @@
+//decode
+function pkdecode() {
+    var urimod = decodeURIComponent(document.getElementById("uristage").value); //input
+    var decodedData = window.atob(urimod); //decoding the string
+    decodedData = decodeURIComponent(decodedData); //URI decoding for special characters
+    document.getElementById("uristage").value = decodedData; //output
+}
+
+//encode
+function pkencode() {
+    var urimod = encodeURIComponent(document.getElementById("uristage").value); //input
+    var encodedData = encodeURIComponent(urimod); //URI encoding for special characters
+    encodedData = window.btoa(urimod); //encoding the string    
+    document.getElementById("uristage").value = encodedData; //output
+}
+
+//clear
+function clearitall() {
+    document.getElementById("uristage").value = ""; //clearing the stage (textarea)
+}
